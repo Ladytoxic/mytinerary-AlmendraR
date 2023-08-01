5b5d106a463db0884f178145f8c5faccb58dff58
Almendra Romina R <almendraromina1@gmail.com>
2023-07-31 21:06:02 -0300

    Inicio de proyecto con React

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './footer.css'
+
+function Footer() {
+    return (
+        <>
+            <footer>
+                <h4>Todos los derechos Reservados</h4>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <>
+            <nav>
+                <a href="/">My Tinerary</a>
+                <ul className='Menu'>
+                    <li><a href="">Link</a></li>
+                    <li><a href="">Link</a></li>
+                    <li><a href="">Link</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+export default Navbar
