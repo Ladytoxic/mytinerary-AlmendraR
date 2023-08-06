60105b47445644c5671a833b96e5a02d80d4145c
Almendra Romina R <almendraromina1@gmail.com>
2023-08-06 01:38:13 -0300

    icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,24 @@
 import './Navbar.css'
 import LinksList from '../LinksList/LinksList'
+import icon from '../../../public/Icon.svg'
+import { Person } from 'akar-icons';
 
 function Navbar() {
     const links = [
         { title: 'Home', to: '/' },
         { title: 'Cities', to: '/cities' },
     ]
+
+    const login = () => {
+        window.location.href = '/login';
+    }
+
     return (
-        <nav>
-            <a className='navbar-title' href="/">MyTinerary</a>
+        <nav className='fade-in-top'>
+            <a className='navbar-title' href="/"><img className='icon' src={icon} alt="icon" />MyTinerary</a>
             <div className='menu'>
                 <LinksList links={links} className='item-links-nav' />
-                <button className='btn-login'>Login</button>
+                <button className='btn-login' onClick={login}><Person strokeWidth={3} size={16} /> Login</button>
             </div>
         </nav>
     )
