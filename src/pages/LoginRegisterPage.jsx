34d9755155952f835047c0a855812859b69bc646
Almendra Romina R <almendraromina1@gmail.com>
2023-09-06 22:36:34 -0300

    Component login, register

--- src/pages/LoginRegisterPage.jsx
+++ src/pages/LoginRegisterPage.jsx
@@ -1,9 +1,23 @@
+import { ArrowLeft } from 'akar-icons';
+import icon from '../assets/undraw_world_re_768g.svg';
+import Login from '../components/Login/Login';
 const LoginRegisterPage = () => {
   return (
-    <div className="loginPage">
-      <h1>Sing in</h1>
-      <h2>🚧Page under construction🚧</h2>
-    </div>
+    <>
+      <div className="loginPage">
+        <header className='loginHeader'>
+          <div className='navbar-signIn'>
+            <button className='btn-goBack link'>
+              <ArrowLeft strokeWidth={2.5} size={36} />
+            </button>
+          </div>
+          <img className='icon-login' src={icon} alt="icon-myTinerary" />
+        </header>
+        <Login />
+        <h1>Sign in</h1>
+        <h2>🚧Page under construction🚧</h2>
+      </div>
+    </>
   )
 }
 
