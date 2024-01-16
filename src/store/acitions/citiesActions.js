ed77c2b9eb5589e305309727f47b3c6ee1a18f71
Almendra Romina R <almendraromina1@gmail.com>
2024-01-16 15:36:26 -0300

    error city id

--- src/store/acitions/citiesActions.js
+++ src/store/acitions/citiesActions.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import apiUrl from '../../api';
+import apiUrl from '../../api'
 
 export const get_cities = createAsyncThunk('get_cities', async () => {
     try {
@@ -28,7 +28,7 @@ export const filter_cities = createAsyncThunk('filter_cities', async (obj) => {
 
 export const get_city = createAsyncThunk('get_city', async (obj) => {
     try {
-        const response = await axios.get(`${apiUrl}/cities?name=${obj.id}`)
+        const response = await axios.get(`${apiUrl}/cities/${obj.id}`)
         return {
             city: response.data.city
         }
